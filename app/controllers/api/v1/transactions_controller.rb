58470b74e4b888d05b05fe6a1b2943551d496d7b
marlondevelopment <marlongonzalezgomez16@gmail.com>
2024-08-13 16:47:01 -0500

    :heavy_plus_sign: add user and transaction controllers

---
+++ app/controllers/api/v1/transactions_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::TransactionsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::UsersController < ApplicationController
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+end
